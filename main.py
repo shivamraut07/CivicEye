2efbdc2f9e6ded82e0707bf96e0d0191893ca178
Shivam Raut <shivamraut5014@gmail.com>
2026-03-11 21:51:50 +0530

    v3: added video and phone IP camera input support

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import cv2
 from detection.vehicle_detection import detect_vehicles
 
-# open laptop webcam
-cap = cv2.VideoCapture(0)
+#SOURCE = "webcam"
+#SOURCE = "video"
+SOURCE = "phone"
+
+cap = cv2.VideoCapture("videos/traffic.mp4")
+if SOURCE == "webcam":
+    cap = cv2.VideoCapture(0)
+
+elif SOURCE == "video":
+    cap = cv2.VideoCapture("videos/traffic.mp4")
+
+elif SOURCE == "phone":
+    cap = cv2.VideoCapture("http://10.245.75.220:8080/video")
 
 while True:
 
@@ -21,11 +32,11 @@ while True:
 
         text = f"{label} {conf:.2f}"
 
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
-        cv2.putText(frame, text, (x1, y1-10),
+        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
+        cv2.putText(frame, text, (x1,y1-10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
 
-    cv2.imshow("CivicEye - Vehicle Detection", frame)
+    cv2.imshow("CivicEye Demo", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
